66626d3df8c2d5c07e54f49a9b2bf0676c3acf81
Carlos Ivanchuk <buhodev@proton.me>
2023-07-15 11:44:16 -0400

    add melt-ui with preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import sequence from 'svelte-sequential-preprocessor';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocessMeltUI } from '@melt-ui/pp';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
