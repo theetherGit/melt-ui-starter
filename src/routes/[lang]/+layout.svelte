2c7e5129b30d94ba4f934ab5de96fe133d5f04b2
Carlos Ivanchuk <buhodev@proton.me>
2023-07-25 07:52:04 -0400

    Move LanguageSwitcher component to the layout

---
+++ src/routes/[lang]/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import LanguageSwitcher from './LanguageSwitcher.svelte';
+
+	export let data;
+</script>
+
+<LanguageSwitcher />
+
+<slot />
--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,9 +1,6 @@
 <script>
 	import { i } from '@inlang/sdk-js';
-	import LanguageSwitcher from './LanguageSwitcher.svelte';
 </script>
-
-<LanguageSwitcher />
 
 <h1>{i('welcome')}</h1>
 <p>{@html i('description')}</p>
