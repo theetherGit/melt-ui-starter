cd682a6be89fa32a6a1203241a83f85d2d3a8f79
Shivam Meena <meenashivam9650@gmail.com>
2023-09-28 11:32:00 +0530

    as dev

--- src/routes/[lang]/og/+server.ts
+++ src/routes/[lang]/og/+server.ts
@@ -3,14 +3,14 @@ import OG from './OG.svelte';
 import type {RequestHandler} from "@sveltejs/kit";
 
 const WIDTH = 1200;
-const HEIGHT = 630;
+const HEIGHT = 1200;
 
 export const GET: RequestHandler = async ({ url }) => {
 	const text = url.searchParams.get('text') ?? undefined;
 	const spanText = url.searchParams.get('spanText') ?? undefined;
-	const width = Number(url.searchParams.get('w')) ?? WIDTH;
-	const height = Number(url.searchParams.get('h')) ?? HEIGHT;
-
+	const width = Number(url.searchParams.get('w') ?? 1200);
+	const height = Number(url.searchParams.get('h') ?? 1200);
+	console.log(url.searchParams.get('h'), width);
 	return await componentToImageResponse(
 		OG,
 		{ text, spanText },
